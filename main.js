415be49cbf66784554e9ff1de404e6b21f183ef5
nrharu <kuuasr8943@gmail.com>
2021-06-24 15:59:22 +0900

    js途中経過

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
-const searchbox_open_button = document.getElementsByName("searchbox_open");
-const searchbox = document.getElementsByClassName("button_serchbox_wrap");
-searchbox.onclick = function changeContent() {
-  searchbox.classlist.toggle(
-    "button_searchbox_wrap_open",
-    "button_searchbox_wrap"
-  );
-  //   return "ha";
+const searchbox_open_button = document.getElementById("js-searchbox-open");
+const searchbox = document.getElementById("js-button-searchbox-wrap");
+searchbox_open_button.onclick = () => {
+  searchbox.classList.toggle("button_searchbox_wrap_open");
+  searchbox.classList.toggle("button_searchbox_wrap");
 };
-// console.log(button_searchbox_open.onclick);
-// const target = document.getElementsByClassName("buttan_serchbox_wrap");
-
-// target.addEventListener("click", function () {
-//   this.classList.toggle("radius");
-// });
+const hamburger_wrap = document.getElementById("js_hamburger");
+const sidemenu_close = document.getElementById("js_sidemenu_close_space");
+const sidemenu = document.getElementById("js_sidemenu");
+hamburger_wrap.onclick = () => {
+  sidemenu.classList.toggle("sidemenu");
+  sidemenu.classList.toggle("sidemenu_open");
+  sidemenu_close.classList.add("sidemenu_close_space");
+};
+sidemenu_close.onclick = () => {
+  sidemenu.classList.toggle("sidemenu");
+  sidemenu.classList.toggle("sidemenu_open");
+};
+// const sidemenu_list = document.getElementsByClassName("js_sidemenu_list");
+// let index =sidemenu_list
+// let remainder_index =
+// sidemenu_list[0].onclick = function () {
+//     sidemenu_list[remainder_index].removeAttribute("open");
+// sidemenu_list[0].onclick = function () {
+//   sidemenu_list[1].removeAttribute("open");
+//   return "rabbit";
+const sidemenu_list = document.getElementsByClassName("js_sidemenu_list");
+let index;
+sidemenu_list.forEach((sidemenu) => {
+  sidemenu.addEventListener("click", () => {
+    index = [].slice.call(sidemenu_list).indexOf(sidemenu);
+  });
+});
