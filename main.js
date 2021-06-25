b08c01ee92feda788f9a541426f952421edb6b96
nrharu <kuuasr8943@gmail.com>
2021-06-25 11:44:39 +0900

    jsiconanimation

--- main.js
+++ main.js
@@ -7,27 +7,78 @@ searchbox_open_button.onclick = () => {
 const hamburger_wrap = document.getElementById("js_hamburger");
 const sidemenu_close = document.getElementById("js_sidemenu_close_space");
 const sidemenu = document.getElementById("js_sidemenu");
+const hamburger_parts = document.getElementsByClassName("js_hamburger_parts");
 hamburger_wrap.onclick = () => {
   sidemenu.classList.toggle("sidemenu");
   sidemenu.classList.toggle("sidemenu_open");
-  sidemenu_close.classList.add("sidemenu_close_space");
+  sidemenu_close.classList.toggle("sidemenu_close_space");
+  hamburger_parts[0].classList.toggle("js_hamburger_parts_1");
+  hamburger_parts[1].classList.toggle("js_hamburger_parts_2");
+  hamburger_parts[2].classList.toggle("js_hamburger_parts_3");
 };
 sidemenu_close.onclick = () => {
   sidemenu.classList.toggle("sidemenu");
   sidemenu.classList.toggle("sidemenu_open");
+  sidemenu_close.classList.toggle("sidemenu_close_space");
 };
-// const sidemenu_list = document.getElementsByClassName("js_sidemenu_list");
-// let index =sidemenu_list
-// let remainder_index =
-// sidemenu_list[0].onclick = function () {
-//     sidemenu_list[remainder_index].removeAttribute("open");
-// sidemenu_list[0].onclick = function () {
-//   sidemenu_list[1].removeAttribute("open");
-//   return "rabbit";
 const sidemenu_list = document.getElementsByClassName("js_sidemenu_list");
-let index;
-sidemenu_list.forEach((sidemenu) => {
-  sidemenu.addEventListener("click", () => {
-    index = [].slice.call(sidemenu_list).indexOf(sidemenu);
-  });
-});
+// const open_sidemenu_list =
+// const new_sidemenu_list = sidemenu_list.filter((n) => n !== "");
+// console.log(`new_sidemenu_list=${new_sidemenu_list}`);
+//関数考える
+sidemenu_list[0].onclick = () => {
+  sidemenu_list[1].removeAttribute("open");
+  sidemenu_list[2].removeAttribute("open");
+  sidemenu_list[3].removeAttribute("open");
+  sidemenu_list[4].removeAttribute("open");
+};
+sidemenu_list[1].onclick = () => {
+  sidemenu_list[2].removeAttribute("open");
+  sidemenu_list[3].removeAttribute("open");
+  sidemenu_list[4].removeAttribute("open");
+  sidemenu_list[0].removeAttribute("open");
+};
+sidemenu_list[2].onclick = () => {
+  sidemenu_list[3].removeAttribute("open");
+  sidemenu_list[4].removeAttribute("open");
+  sidemenu_list[0].removeAttribute("open");
+  sidemenu_list[1].removeAttribute("open");
+};
+sidemenu_list[3].onclick = () => {
+  sidemenu_list[4].removeAttribute("open");
+  sidemenu_list[0].removeAttribute("open");
+  sidemenu_list[1].removeAttribute("open");
+  sidemenu_list[2].removeAttribute("open");
+};
+sidemenu_list[4].onclick = () => {
+  sidemenu_list[0].removeAttribute("open");
+  sidemenu_list[1].removeAttribute("open");
+  sidemenu_list[2].removeAttribute("open");
+  sidemenu_list[3].removeAttribute("open");
+};
+
+const information_list = document.getElementsByClassName("information_list");
+information_list[0].onclick = () => {
+  information_list[1].removeAttribute("open");
+  information_list[2].removeAttribute("open");
+  information_list[3].removeAttribute("open");
+};
+information_list[1].onclick = () => {
+  information_list[2].removeAttribute("open");
+  information_list[3].removeAttribute("open");
+  information_list[0].removeAttribute("open");
+};
+information_list[2].onclick = () => {
+  information_list[3].removeAttribute("open");
+  information_list[0].removeAttribute("open");
+  information_list[1].removeAttribute("open");
+};
+information_list[3].onclick = () => {
+  information_list[0].removeAttribute("open");
+  information_list[1].removeAttribute("open");
+  information_list[2].removeAttribute("open");
+};
+
+const items = ["りんご", "オレンジ", "バナナ"];
+const newItems = items.filter((n) => n !== "りんご");
+console.log(`newItems = ${newItems}`);
