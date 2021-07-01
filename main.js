963e76f824bf42de6dd12fff57621ac304f4f5fb
nrharu <kuuasr8943@gmail.com>
2021-07-01 18:57:37 +0900

    iconclasschange

--- main.js
+++ main.js
@@ -2,12 +2,21 @@
  * 検索ボタンを押して出現
  */
 
-const searchbox_open_button = document.getElementById("js_searchbox_open");
+const searchbox_open_button = document.getElementById("js_button_open_icon");
 const searchbox = document.getElementById("js_button_searchbox_wrap");
-
 searchbox_open_button.onclick = () => {
   searchbox.classList.toggle("button_searchbox_wrap_open");
-  searchbox.classList.toggle("button_searchbox_wrap");
+};
+
+/**
+ * 検索ボックスを閉じるボタン
+ */
+
+const searchbox_close_icon = document.getElementById(
+  "js_searchbox_close_button"
+);
+searchbox_close_icon.onclick = () => {
+  searchbox.classList.toggle("button_searchbox_wrap_open");
 };
 
 /**
@@ -23,21 +32,15 @@ const sidemenu_open = () => {
   sidemenu.classList.toggle("sidemenu");
   sidemenu.classList.toggle("sidemenu_open");
   sidemenu_close.classList.toggle("sidemenu_close_space");
-  for (i = 0; i < hamburger_wrap.length; i++)
-    hamburger_parts[i].classList.toggle(`js_hamburger_parts_${i + 1}`);
-  hamburger_parts[i].classList.toggle(`js_hamburger_parts_${i + 1}_reverse`);
-
-  // hamburger_parts[0].classList.toggle("js_hamburger_parts_1");
-  // hamburger_parts[1].classList.toggle("js_hamburger_parts_2");
-  // hamburger_parts[2].classList.toggle("js_hamburger_parts_3");
-  // hamburger_parts[0].classList.toggle("js_hamburger_parts_1_reverse");
-  // hamburger_parts[1].classList.toggle("js_hamburger_parts_2_reverse");
-  // hamburger_parts[2].classList.toggle("js_hamburger_parts_3_reverse");
+  // for (i = 0; i < hamburger_wrap.length; i++) {
+  //   hamburger_parts[i].classList.toggle(`js_hamburger_parts_${i + 1}`);
+  //   hamburger_parts[i].classList.toggle(`js_hamburger_parts_${i + 1}_reverse`);
+  // }
+  hamburger_parts[0].classList.toggle("js_hamburger_parts_1");
+  hamburger_parts[0].classList.toggle("js_hamburger_parts_1_reverse");
+  // hamburger_parts[0].classList.toggle("js_hamburger_parts_disabled");
+  // hamburger_parts[0].classList.toggle("js_hamburger_parts_disabled_reverse");
 };
-for (i = 0; i < hamburger_wrap; i++)
-  hamburger_parts[i].classList.toggle(`js_hamburger_parts_${i + 1}`);
-hamburger_parts[i].classList.toggle(`js_hamburger_parts_${i + 1}_reverse`);
-
 hamburger_wrap.onclick = () => {
   sidemenu_open();
 };
@@ -81,6 +84,23 @@ handleAccordion("js_sidemenu_list");
  * ドロワー内のアコーディオン子階層
  */
 handleAccordion("sidemenu_list_child");
+
+/**
+ * login_linkのホバー時のアイコンの色変更
+ */
+const login_link_icon_color = document.getElementById(
+  "js_login_link_icon_color"
+);
+const login_link_wrap = document.getElementById("js_login_link_wrap");
+console.log(login_link_icon_color);
+console.log(login_link_wrap);
+login_link_wrap.addEventListener("mouseover", () => {
+  login_link_icon_color.classList.add("js_login_link_icon_color_hover");
+  console.log("Hello world");
+});
+login_link_wrap.addEventListener("mouseout", () => {
+  login_link_icon_color.classList.remove("js_login_link_icon_color_hover");
+});
 
 /**
  * footerのアコーディオン
